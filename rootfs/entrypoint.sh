723fd88634b236c80696693746904a1f2a5a6ddd
Phi Pham <pham.pphi@gmail.com>
2023-06-23 13:55:03 +1000

    Update Dockerfile and entrypoint.sh

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export PATH=$PATH:/usr/bin/cpd-cli
-cpd-cli config users set cpd-admin --username ${CPD_API_USER} --apikey ${CPD_API_KEY}
-cpd-cli config profiles set ${CPD_PROFILE} --user cpd-admin --url ${CPD_PROFILE_URL}
+#cpd-cli config users set cpd-admin --username ${CPD_API_USER} --apikey ${CPD_API_KEY}
+#cpd-cli config profiles set ${CPD_PROFILE} --user cpd-admin --url ${CPD_PROFILE_URL}
 
 # This will exec the CMD from your Dockerfile
 exec "$@"
